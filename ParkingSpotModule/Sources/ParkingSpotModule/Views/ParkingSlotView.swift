7413d0df34075139800bf4aab8ad13f2ccd62038
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2023-01-05 13:51:32 +0200

    Removed code that was autogenrated by the module, including here: `ParkingSpotModule` & `ParkingSpotModuleTests`. Created `ParkingSlotView` to be responsible for creating and disaplaying the parking slot overview.

---
+++ ParkingSpotModule/Sources/ParkingSpotModule/Views/ParkingSlotView.swift
@@ -0,0 +1,20 @@
+//
+//  ParkingSlotView.swift
+//  
+//
+//  Created by Vlad Grigore Sima on 05.01.2023.
+//
+
+import SwiftUI
+
+struct ParkingSlotView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ParkingSlotView_Previews: PreviewProvider {
+    static var previews: some View {
+        ParkingSlotView()
+    }
+}
