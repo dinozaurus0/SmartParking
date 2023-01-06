f795709bea1924829899b79d756a7d5dd45ad5e5
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2023-01-06 13:13:46 +0200

    Updated `ParkingSlotView` to display a `Grid` of n*m elements with random colors selected from an predefined array. This is intented solely to emulate the matrix needed to display the parking layout which will be driven by backend.

--- ParkingSpotModule/Sources/ParkingSpotModule/Views/ParkingSlotView.swift
+++ ParkingSpotModule/Sources/ParkingSpotModule/Views/ParkingSlotView.swift
@@ -7,14 +7,34 @@
 
 import SwiftUI
 
-struct ParkingSlotView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct ParkingSlotView: View {
+    private let colors: [Color] = [.blue, .red, .green, .purple, .pink, .orange, .gray, .cyan, .black, .white, .brown]
+
+    public var body: some View {
+        Grid(horizontalSpacing: 0, verticalSpacing: 0, content: {
+            ForEach(0..<80, content: { _ in
+                GridRow {
+                    ForEach(0..<80) { _ in
+                        let index = Int.random(in: 0..<colors.count)
+                        colors[index]
+                    }
+                }
+            })
+        })
+        .ignoresSafeArea()
     }
 }
 
-struct ParkingSlotView_Previews: PreviewProvider {
-    static var previews: some View {
+internal struct ParkingSlotView_Previews: PreviewProvider {
+    internal static var previews: some View {
         ParkingSlotView()
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
+
+/*
+ TODO:
+ 1. Create the grid that is to be a matrix of n*m elements.
+ n*m matrix -->
+ 2. Force the screen to rotate in landscape when this screen is entered. (this will be done when linked with the main module and the coordinators if there will be any)
+ */
